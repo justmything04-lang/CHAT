e8e69f36dd0fc5c6f5404d93f2d2182f783b5d60
justmything04-lang <justmything04@gmail.com>
2025-10-08 15:24:46 +0530

    Update CHANGE.py

--- CHANGE.py
+++ CHANGE.py
@@ -1,7 +1,2 @@
-# Handle EasterEgg + RegID (works for both modes)
-@bot.message_handler(func=lambda m:
-    m.text is not None
-    and not m.text.startswith('/')
-    and str(m.from_user.id) not in pending_change   # <-- prevents hijacking "change egg"
-    and str(m.from_user.id) not in pending_time_change  # <-- prevents hijacking "change time"
-    )
+online_attendance_sheet = client.open_by_key(SHEET_ID).worksheet(ONLINE_ATTENDANCE_TAB)
+online_master_sheet = client.open_by_key(SHEET_ID).worksheet(ONLINE_MASTER_TAB)
